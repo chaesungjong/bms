29bc2c9cc0c1356422d29f5c24c48fe54dcdb8f7
Castlebell <chae_s_j@naver.com>
2024-04-23 20:04:21 +0900

    first commit

---
+++ src/main/java/com/groupd/bms/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.groupd.bms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index" ;
+    }
+    
+}
