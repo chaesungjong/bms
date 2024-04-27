53be978c81c785958cac723505dd37e12be43696
Castlebell <chae_s_j@naver.com>
2024-04-27 14:37:33 +0900

    mssql 및 타임리프 적용

--- src/main/java/com/groupd/bms/controller/IndexController.java
+++ src/main/java/com/groupd/bms/controller/IndexController.java
@@ -1,15 +1,20 @@
 package com.groupd.bms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+/**
+ * IndexController
+ * 시작 페이지를 보여주기 위한 컨트롤러
+ */
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "index" ;
+    public String index(Model model) {
+        model.addAttribute("title", "테스트"); // `index.html`에서 사용될 타이틀
+        return "index"; // `index.html` 템플릿 반환
     }
     
 }
