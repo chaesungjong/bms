47da3d8c78b8fb0d962c22b2ecd1f7f741a21f56
강성진 <wlsdljuam@nate.com>
2024-07-16 12:06:30 +0900

    sidebar member info 퍼블리싱 추가

--- src/main/resources/static/js/templates/partials/sidebar.js
+++ src/main/resources/static/js/templates/partials/sidebar.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
-
-  $('.side_menu > li > a').click(function (e) {
-      e.preventDefault();
-      $(this).toggleClass('open');
-      $('.side_menu > li > a').not(this).removeClass('open');
-      var subMenu = $(this).siblings('.side_sub_menu');
-      $('.side_sub_menu').not(subMenu).slideUp();
-      subMenu.slideToggle();
-  });
+    $('.side_menu > li > a').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('open');
+        $('.side_menu > li > a').not(this).removeClass('open');
+        var subMenu = $(this).siblings('.side_sub_menu');
+        $('.side_sub_menu').not(subMenu).slideUp();
+        subMenu.slideToggle();
+    });
 
     // a 태그 클릭 이벤트를 바인딩합니다.
-    $("a[data-ajax]").click(function(event){
+    $('a[data-ajax]').click(function (event) {
         event.preventDefault();
-        var url = $(this).attr("href");
+        var url = $(this).attr('href');
         loadContent(url);
     });
+});
+$('.user_info_btn').click(function () {
+    var userBtn = $(this).attr('id');
+    $('.member_info_wrap').removeClass('on');
+    $('.member_info_wrap').removeClass('out');
+    $('.member_info_wrap#' + userBtn).addClass('on');
+    $('body, html').addClass('modal-active');
+});
+
+$('.member_info_close').click(function () {
+    $('.member_info_wrap').addClass('out');
+    $('body, html').removeClass('modal-active');
 });
