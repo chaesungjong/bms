6a9c4906e19fd9490788e155363179595efa78a3
강성진 <wlsdljuam@nate.com>
2024-07-16 17:29:31 +0900

    유지보수 리스트 1차 퍼블리싱, 사원등록 작성중

--- src/main/resources/static/js/templates/csf/maintenance.js
+++ src/main/resources/static/js/templates/csf/maintenance.js
@@ -2,5 +2,18 @@
  * csf/maintenance.html 연동 스크립트 영역
  */
 $(document).ready(function () {
-
+    $('.main_board_state>span').click(function () {
+        $('.main_board_state>span').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.main_board_category>span').click(function () {
+        $('.main_board_category>span').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.main_board_wrstate').click(function () {
+        $(this).children('.state_select').slideToggle(100);
+    });
+    $('.main_board_wrcategory').click(function () {
+        $(this).children('.category_select').slideToggle(100);
+    });
 });
