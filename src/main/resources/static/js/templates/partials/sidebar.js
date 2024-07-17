8caf035ae176442b43b6f18267eaedecc20b6077
강성진 <wlsdljuam@nate.com>
2024-07-17 18:02:22 +0900

    업체등록 퍼블리싱중

--- src/main/resources/static/js/templates/partials/sidebar.js
+++ src/main/resources/static/js/templates/partials/sidebar.js
@@ -14,16 +14,16 @@ $(document).ready(function () {
         var url = $(this).attr('href');
         loadContent(url);
     });
-});
-$('.user_info_btn').click(function () {
-    var userBtn = $(this).attr('id');
-    $('.member_info_wrap').removeClass('on');
-    $('.member_info_wrap').removeClass('out');
-    $('.member_info_wrap#' + userBtn).addClass('on');
-    $('body, html').addClass('modal-active');
-});
+    $('.user_info_btn').click(function () {
+        var userBtn = $(this).attr('id');
+        $('.member_info_wrap').removeClass('on');
+        $('.member_info_wrap').removeClass('out');
+        $('.member_info_wrap#' + userBtn).addClass('on');
+        $('body, html').addClass('modal-active');
+    });
 
-$('.member_info_close').click(function () {
-    $('.member_info_wrap').addClass('out');
-    $('body, html').removeClass('modal-active');
+    $('.member_info_close').click(function () {
+        $('.member_info_wrap').addClass('out');
+        $('body, html').removeClass('modal-active');
+    });
 });
