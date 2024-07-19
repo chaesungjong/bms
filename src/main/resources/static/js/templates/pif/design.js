94d8d2bc0246c413bdbf855678e3815267c22c3e
강성진 <wlsdljuam@nate.com>
2024-07-19 18:01:02 +0900

    프로젝트 목록 (디자인) 1차 퍼블리싱

--- src/main/resources/static/js/templates/pif/design.js
+++ src/main/resources/static/js/templates/pif/design.js
@@ -2,5 +2,22 @@
  * pif/design.html 연동 스크립트 영역
  */
 $(document).ready(function () {
-
+    $('.design_board_type>span').click(function () {
+        $('.design_board_type>span').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.main_board_state>span').click(function () {
+        $('.main_board_state>span').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.design_board_category>span').click(function () {
+        $('.design_board_category>span').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.main_board_wrstate').click(function () {
+        $(this).children('.state_select').slideToggle(100);
+    });
+    $('.main_board_wrcategory').click(function () {
+        $(this).children('.category_select').slideToggle(100);
+    });
 });
