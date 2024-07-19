a278862f863bb323d0d0e43f05f6892a194c22fb
강성진 <wlsdljuam@nate.com>
2024-07-19 10:13:41 +0900

    사원목록조회 1차 퍼블리싱

--- src/main/resources/static/js/templates/ems/employees.js
+++ src/main/resources/static/js/templates/ems/employees.js
@@ -1,6 +1,15 @@
 /*
  * ems/employees.html 연동 스크립트 영역
  */
-$(document).ready(function () {
+$('.user_info_btn').click(function () {
+    var userBtn = $(this).attr('id');
+    $('.member_info_wrap').removeClass('on');
+    $('.member_info_wrap').removeClass('out');
+    $('.member_info_wrap#' + userBtn).addClass('on');
+    $('body, html').addClass('modal-active');
+});
 
+$('.member_info_close').click(function () {
+    $('.member_info_wrap').addClass('out');
+    $('body, html').removeClass('modal-active');
 });
