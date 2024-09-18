5d4a0bd4ca3241823f535ec8baa84c8e262f6a33
castlebell <chaesj1991@gmail.com>
2024-09-18 22:34:49 +0900

    불필요한 파일 삭제

--- src/main/java/com/groupd/bms/controller/Enterprise/dsb/EnterpriseController.java
+++ src/main/java/com/groupd/bms/controller/Enterprise/dsb/EnterpriseController.java
@@ -1,4 +1,4 @@
-package com.groupd.bms.controller.enterprise;
+package com.groupd.bms.controller.enterprise.dsb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
