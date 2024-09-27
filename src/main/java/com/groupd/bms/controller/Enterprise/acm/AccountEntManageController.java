04f7a19693bdee546c703bea7753f7aa591e37f4
ddasik <ddasik8352@gmail.com>
2024-09-27 22:00:15 +0900

    외부용 로그인/로그인처리

---
+++ src/main/java/com/groupd/bms/controller/Enterprise/acm/AccountEntManageController.java
@@ -0,0 +1,84 @@
+package com.groupd.bms.controller.Enterprise.acm;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.groupd.bms.controller.BaseController;
+import com.groupd.bms.model.MemberLogin;
+import com.groupd.bms.service.UserService;
+import com.groupd.bms.util.SHA256Util;
+import com.groupd.bms.util.Util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * AccountEntManageController
+ * 업체 계정 관리 페이지를 보여주기 위한 컨트롤러
+ */
+@Controller
+@RequestMapping("enterprise/acm")
+public class AccountEntManageController extends BaseController{
+    
+    @Autowired
+    private UserService userService;
+
+    /*
+     * 프로젝트 정보 화면 페이지 
+     */
+    @GetMapping("/{path}")
+    public String handleSinglePath(@PathVariable("path") String path, Model model) {
+        return "enterprise/acm/" + path;
+    }
+
+    /*
+     * 로그인을 시도한다.
+     * @param username
+     * @param password
+     * @return ModelAndView
+     */
+    @RequestMapping(value="/loginProcess.do", method = { RequestMethod.POST, RequestMethod.GET })
+    public ResponseEntity<?> login(@RequestParam("userId") String userID, @RequestParam("password") String password, HttpServletRequest request) {
+
+        // 로그인 시도
+        HashMap<String, Object> loginResultMap = userService.login(new MemberLogin(userID, SHA256Util.hashWithSHA256(password), Util.getUserIP(request), "DT"));
+
+        // 로그인 성공 시 세션에 회원 정보를 저장
+        if (loginResultMap != null){
+
+            if(loginResultMap.get("member") != null)
+                request.getSession().setAttribute("member", loginResultMap.get("member"));
+            
+            // 로그인 결과 반환
+            return ResponseEntity.ok(loginResultMap);
+        }
+
+        // 로그인 실패 시 500 에러 반환
+        else return ResponseEntity.status(500).build();
+
+    }
+
+    /*
+     * 로그아웃을 시도한다.
+     * @param request
+     * @return ModelAndView
+     * @throws Exception
+     */
+    @RequestMapping(value="/logout.do", method = { RequestMethod.POST, RequestMethod.GET })
+    public ResponseEntity<?> logout(HttpServletRequest request) throws Exception {
+        // 세션에서 회원 정보 삭제
+        request.getSession().removeAttribute("member");
+        HashMap<String, Object> loginoutMap = new HashMap<String, Object>();
+        loginoutMap.put("result", "success");
+        return ResponseEntity.ok(loginoutMap);
+    }
+        
+}
---
+++ src/main/resources/static/js/templates/enterprise/acm/login.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    // 페이지 로드 시 저장된 아이디 로드
+    var savedUserId = localStorage.getItem('userId');
+    if (savedUserId) {
+        $('#userId').val(savedUserId);
+        $('#rememberMe').prop('checked', true);
+    }
+
+    /**
+     * 로그인 버튼
+     */
+    $("#loginBtn").click(function () {
+        var username = $('#userId').val();
+        var password = $('#password').val();
+
+        // 유효성 검사
+        if (username.trim() === '' || password.trim() === '') {
+            alert('로그인 정보를 정확히 입력해주세요.');
+            return;
+        }
+
+        if (forbiddenWordsCheck(username) || forbiddenWordsCheck(password)) {
+            alert('금지된 단어가 포함되어 있습니다.');
+            return;
+        }
+
+        // 아이디 저장 체크박스 검사
+        if ($('#rememberMe').is(':checked')) {
+            localStorage.setItem('userId', username); // 로컬 스토리지에 아이디 저장
+        } else {
+            localStorage.removeItem('userId'); // 저장된 아이디 삭제
+        }
+
+        // 자동 로그인 체크박스 검사
+        if ($('#autoLogin').is(':checked')) {
+            // 예제 사용법
+            setCookie("autoLogin", "true", 365);
+            setCookie("userId", username, 365);
+            setCookie("password", password, 365);
+        } else {
+            document.cookie = "autoLogin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
+
+        // 로그인 데이터 준비
+        var loginData = {
+            userId: username,
+            password: password
+        };
+
+        // ajaxRequest 함수를 사용하여 로그인 처리
+        ajaxRequest("/enterprise/acm/loginProcess.do", loginData, "POST", function (response) {
+            // 로그인 성공 시 대시보드 페이지로 리디렉트
+            alert("Aaa")
+            if (response.retVal == 0) {
+                location.href = "/admin/dsb/main";
+            } else {
+                alert(response.errMsg);
+            }
+        }, function () {
+            //error.status 분기에 따른 오류메세지 노출 필요
+            alert('로그인 처리중 오류가 발생하였습니다. 재 시도 해주시기 바랍니다.');
+        });
+    });
+});
