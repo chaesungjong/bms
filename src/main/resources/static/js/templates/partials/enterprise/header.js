2eebe70b100f24d4354afc75ada34ffd24df5fd2
강성진 <wlsdljuam@nate.com>
2024-10-24 18:00:22 +0900

    외부공통헤더 업데이트

--- src/main/resources/static/js/templates/partials/enterprise/header.js
+++ src/main/resources/static/js/templates/partials/enterprise/header.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
+    var $modalBg = $('.modal_bg');
+    var $hdSearchModal = $('.hd_search_modal');
+    var $hdTop = $('.hd_top');
 
+    function toggleModal(open) {
+        $modalBg.toggleClass('open', open);
+        $hdSearchModal.toggleClass('open', open);
+    }
+
+    $('.hd_search_btn').click(function () {
+        toggleModal(true);
+    });
+
+    $modalBg.add('.search_modal_close').click(function () {
+        toggleModal(false);
+    });
 });
